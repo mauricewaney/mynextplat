fa3223478fac4a07a5dfd17376f4736c7ef9787a
mwaneykm <m.waney@keukenmagazijn.nl>
2026-03-09 19:21:35 +0100

    trophy counter sizing 2

--- resources/js/components/GameCard.vue
+++ resources/js/components/GameCard.vue
@@ -83,10 +83,10 @@
                         <span v-if="game.gold_count" class="inline-flex items-center gap-0.5 text-xs font-bold text-yellow-500 dark:text-yellow-400">
                             <TrophyIcon tier="gold" size="xs" />{{ game.gold_count }}
                         </span>
-                        <span v-if="game.silver_count" class="inline-flex items-center gap-0.5 xs font-bold text-gray-400 dark:text-gray-300">
+                        <span v-if="game.silver_count" class="inline-flex items-center gap-0.5 text-xs font-bold text-gray-400 dark:text-gray-300">
                             <TrophyIcon tier="silver" size="xs" />{{ game.silver_count }}
                         </span>
-                        <span v-if="game.bronze_count" class="inline-flex items-center gap-0.5 xs font-bold text-amber-700 dark:text-amber-500">
+                        <span v-if="game.bronze_count" class="inline-flex items-center gap-0.5 text-xs font-bold text-amber-700 dark:text-amber-500">
                             <TrophyIcon tier="bronze" size="xs" />{{ game.bronze_count }}
                         </span>
                     </div>
